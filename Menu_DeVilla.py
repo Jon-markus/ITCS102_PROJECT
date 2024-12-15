143b183d78c7dfaefdd3314674e4a84cfd6649d3
John Marc De Villa <hellloworld.205@gmail.com>
2024-12-16 06:04:09 +0800

    Update Menu_DeVilla.py

--- Menu_DeVilla.py
+++ Menu_DeVilla.py
@@ -1,10 +1,31 @@
 #-----------MENU----------------
 
-def a():
-    pass
+def Overview():
 
-def b():
-    pass
+    print("")
+    print ("\n\t\t\t\t----------OVERVIEW----------")
+    print ("\n\n\tIn 2023, studying programming is not just about coding; it's about equipping")
+    print ("\tyourself with the skills and mindset to thirive in an increasingly digital and")
+    print ("\tinterconnected world. Wheter you aspire to become a software engineer, a data")
+    print ("\tscientist, an entrepreneur, or simply a tech-savvy individual, programming is a")
+    print ("\tskill that empowers you to innovate, solve problems, and seize opportunities in")
+    print ("\tthe digital landscape.") 
+    print("")   
+
+def Introduction():
+
+    import time
+
+    print ("")
+    print("\t\t\t----Python as a Programming Language----")
+    print("\n\ta\t---\tWhat is Python")
+    print("\tb\t---\tPython History")
+    print("\tc\t---\tFeatures of Python")
+    print ("")
+    
+    time.sleep(0.5)
+    Pili = input("\t\tChoose in the following: ")
+
 
 def c():
     pass
@@ -17,3 +38,16 @@ def e():
 
 def f():
     pass
+
+def g():
+    pass
+
+def h():
+    pass
+
+Overview()
+Introduction()
+# isContinue = True
+
+# while isContinue:
+#     pass
